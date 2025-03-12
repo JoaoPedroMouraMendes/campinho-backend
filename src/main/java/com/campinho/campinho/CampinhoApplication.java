528f6509770741727dafde288567e52b329785b6
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-03-11 21:19:31 -0300

    feat: primeiro commit

---
+++ src/main/java/com/campinho/campinho/CampinhoApplication.java
@@ -0,0 +1,13 @@
+package com.campinho.campinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campinho/campinho/CampinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campinho.campinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
