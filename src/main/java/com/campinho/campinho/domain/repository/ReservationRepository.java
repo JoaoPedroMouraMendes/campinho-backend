f343550a9b3953519126e0fbf340fdf451664c26
João Pedro <joaopedromouramendes1@gmail.com>
2025-03-27 16:43:43 -0300

    feat: adiciona método findReservationsNotExpired que busca reservas que estão acontecendo ou vão acontecer

--- src/main/java/com/campinho/campinho/domain/repository/ReservationRepository.java
+++ src/main/java/com/campinho/campinho/domain/repository/ReservationRepository.java
@@ -2,7 +2,14 @@ package com.campinho.campinho.domain.repository;
 
 import com.campinho.campinho.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-public interface ReservationRepository extends JpaRepository<Reservation, UUID> { }
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    @Query("SELECT r FROM Reservation r WHERE r.endTime >= :now")
+    List<Reservation> findReservationsNotExpired(@Param("now") LocalDateTime now);
+}
