7a74f88310c1646c5c462c0f980b54173cd33299
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-04-08 21:23:12 -0300

    chore: remove importações desnecessárias da classe Reservation

--- src/main/java/com/campinho/campinho/domain/entity/Reservation.java
+++ src/main/java/com/campinho/campinho/domain/entity/Reservation.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
